1f9f4312d8530568c6b6628bebae2cd9dbdbdbf8
makiss1956 <makiskomiakitis@gmail.com>
2026-07-31 07:31:26 +0300

    Update frequency_plugin.py

--- plugins/frequency_plugin.py
+++ plugins/frequency_plugin.py
@@ -1,1 +1,22 @@
+"""
+Lottery Intelligence Platform
+Frequency Analysis Plugin
+"""
+
+from collections import Counter
+
+class FrequencyPlugin:
+
+    def __init__(self, name="Frequency Analysis"):
+        self.name = name
+
+    def analyze(self, draws):
+        all_numbers = []
+
+        for draw in draws:
+            all_numbers.extend(draw)
+
+        counts = Counter(all_numbers)
+
+        return sorted(counts.items(), key=lambda x: x[1], reverse=True)
 # Frequency Plugin

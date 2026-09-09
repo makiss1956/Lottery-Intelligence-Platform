dafb3a800fd63a6df4d3d00851f7e5fa6b209526
makiss1956 <makiskomiakitis@gmail.com>
2026-09-09 17:17:01 +0300

    Update __init__.py

--- src/importer/__init__.py
+++ src/importer/__init__.py
@@ -1,1 +1,11 @@
-from .eurojackpot_importer import EurojackpotImporter
+"""
+Importer Package Init
+"""
+
+try:
+    from .eurojackpot_importer import EurojackpotImporter
+except ImportError:
+    try:
+        from .opap_fetcher import OPAPFetcher as EurojackpotImporter
+    except ImportError:
+        EurojackpotImporter = None
